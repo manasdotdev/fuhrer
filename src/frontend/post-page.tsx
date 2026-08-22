dc1051773f42f859c435e6ea11e76b0af8e0d67a
manasdotdev <manasdotdev@gmail.com>
2026-08-22 09:02:57 +0530

    initial commit

---
+++ src/frontend/post-page.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const PostPage: Component = () => {
+  return (
+    <div class="text-center pt-[25vh]">
+      <div>PostPage</div>
+    </div>
+  );
+};
+
+export default PostPage;
